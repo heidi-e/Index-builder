db43fc112d4d8b62d3692945583c6027209a330a
heidi-e <100874463+heidi-e@users.noreply.github.com>
2025-01-30 10:17:46 -0500

    crawl verify dataset for search function

--- indexer/util/search_function.py
+++ indexer/util/search_function.py
@@ -1,19 +1,37 @@
 import json
 
-# Load the JSON file
-with open("data.json", "r") as file:
-    data = json.load(file)
 
-for file in data:
-    # Extract relevant fields
-    title = data.get("title", "No title available")
-    text = data.get("text", "No text available")
-    published_date = data.get("published", "No date available")
-    url = data.get("url", "No URL available")
+def crawl_data(index_struct, file_path):
+    """
+    Crawl folders of news articles and extract metadata
+    Args:
+        index_struct: index structure you are testing
+        file_path: path to the folder containing the dataset
 
-    # Extract preprocessed text
-    preprocessed_text = data.get("preprocessed_text", [])
+    Returns:
 
-    # Print the extracted information
-    print(f"Title: {title}")
-    print(f"Published Date
+    """
+
+    # Load the JSON file
+    with open(file_path, "r") as f:
+        data = json.load(f)
+
+    for file in data:
+
+        # Extract relevant fields
+        title = file["title"]
+        url = file['url']
+        if file['author'] != "":
+            author = file['author'].split('')[1]
+
+        # Extract preprocessed text
+        raw_preprocessed_text = file["preprocessed_text"]
+
+        # store into indexing structure
+        for word in raw_preprocessed_text:
+            index_struct.insert(word, file)
+            index_struct.insert(word, title)
+            index_struct.insert(word, url)
+            index_struct.insert(word, author)
+
+
