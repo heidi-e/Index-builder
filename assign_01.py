029a54896ad3c11e7c69979645c0d44eb56c2806
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-02-01 17:13:10 -0500

    Added in some more functionality to the experiments. Having bugs with json files relating to json file. I know how to fix it though

--- assign_01.py
+++ assign_01.py
@@ -1,6 +1,8 @@
 import json
 from indexer.trees.avl_tree import AVLTreeIndex
 from indexer.trees.bst_index import BinarySearchTreeIndex
+from indexer.trees.bst_index import HashMapIndex
+from indexer.trees.bst_index import ListIndex
 from indexer.util.timer import timer
 from indexer.abstract_index import AbstractIndex
 
@@ -11,13 +13,43 @@ def index_files(path: str, index: AbstractIndex) -> None:
         print(f"path = {path}")
 
     # a sample json news article.  assume this is in a file named sample.json
-    sample_filename = "sample.json"
+    sample_filename = "test2.json"
     sample_json = """
         {
-            "title": "Some article",
-            "text": "here is the text of a sample news article",
-            "preprocessed_text": ["here", "text", "sample", "news", "article"]
-        }
+    "organizations": [],
+    "uuid": "e49cf1a0134f50860eb70214b4416f2cc0715d30",
+    "author": "",
+    "url": "https://www.reuters.com/article/brief-roku-says-youtube-tv-now-available/brief-roku-says-youtube-tv-now-available-on-roku-devices-idUSFWN1PR0WL",
+    "ord_in_thread": 0,
+    "title": "BRIEF-Roku Says Youtube TV Now Available On Roku Devices",
+    "locations": [],
+    "highlightText": "",
+    "language": "english",
+    "persons": [],
+    "text": "Feb 1 (Reuters) - Roku Inc:\n* ROKU INC SAYS YOUTUBE TV NOW AVAILABLE ON ROKU DEVICES Source text for Eikon: Further company coverage:\n ",
+    "published": "2018-02-01T16:16:00.000+02:00",
+    "crawled": "2018-02-01T16:30:53.000+02:00",
+    "highlightTitle": "",
+    "preprocessed_text": [
+        "feb",
+        "reuters",
+        "roku",
+        "inc",
+        "roku",
+        "inc",
+        "say",
+        "youtube",
+        "tv",
+        "available",
+        "roku",
+        "device",
+        "source",
+        "text",
+        "eikon",
+        "company",
+        "coverage"
+    ]
+   }
     """
 
     # extract the preprocessed_text words and add them to the index with
@@ -38,16 +70,16 @@ def loopy_loop():
 def main():
     # You'll need to change this to be the absolute path to the root folder
     # of the dataset
-    data_directory = "/location/of/downloaded/dataset/of/newsarticles"
+    data_directory = "/Users/mihaliskoutouvos/Downloads/final_pickles/hash_index.pkl"
 
     # Here, we are creating a sample binary search tree index object
     # and sending it to the index_files function
-    bst_index = BinarySearchTreeIndex()
-    index_files(data_directory, bst_index)
+    hm_index = HashMapIndex()
+    index_files(data_directory, hm_index)
 
     # As a gut check, we are printing the keys that were added to the
     # index in order.
-    print(bst_index.get_keys_in_order())
+    print(hm_index.get_keys_in_order())
 
     # quick demo of how to use the timing decorator included
     # in indexer.util
--- indexer/util/experiments.py
+++ indexer/util/experiments.py
@@ -6,7 +6,10 @@ from indexer.trees.bst_index import BinarySearchTreeIndex
 from indexer.util.timer import timer
 import random
 import string
+import json
 import pickle
+
+
 
 # Generate random strings
 def generate_random_string(length):
@@ -80,3 +83,88 @@ insert_items(bst_index, 100)
 insert_items(avl_index, 100)
 insert_items(hm_index, 100)
 insert_items(l_index, 100)
+
+
+
+
+def index_files(path: str, index: AbstractIndex) -> None:
+    # path should contain the location of the news articles you want to parse
+    if path is not None:
+        print(f"path = {path}")
+
+    # a sample json news article.  assume this is in a file named sample.json
+    sample_filename = "test2.json"
+    sample_json = """
+    {
+        "organizations": [],
+        "uuid": "e49cf1a0134f50860eb70214b4416f2cc0715d30",
+        "author": "",
+        "url": "https://www.reuters.com/article/brief-roku-says-youtube-tv-now-available/brief-roku-says-youtube-tv-now-available-on-roku-devices-idUSFWN1PR0WL",
+        "ord_in_thread": 0,
+        "title": "BRIEF-Roku Says Youtube TV Now Available On Roku Devices",
+        "locations": [],
+        "highlightText": "",
+        "language": "english",
+        "persons": [],
+        "text": "Feb 1 (Reuters) - Roku Inc:\n* ROKU INC SAYS YOUTUBE TV NOW AVAILABLE ON ROKU DEVICES Source text for Eikon: Further company coverage:\n ",
+        "published": "2018-02-01T16:16:00.000+02:00",
+        "crawled": "2018-02-01T16:30:53.000+02:00",
+        "highlightTitle": "",
+        "preprocessed_text": [
+            "feb",
+            "reute
+            "roku",
+            "inc",
+            "roku",
+            "inc",
+            "say",
+            "youtube",
+            "tv",
+            "available",
+            "roku",
+            "device",
+            "source",
+            "text",
+            "eikon",
+            "company",
+            "coverage"
+     ]
+    }
+    """
+
+    # extract the preprocessed_text words and add them to the index with
+    # sample.json as the file name
+    the_json = json.loads(sample_json)
+    words = the_json["preprocessed_text"]
+
+    for word in words:
+        index.insert(word, sample_filename)
+
+
+# A simple demo of how the @timer decoration can be used
+@timer
+def loopy_loop():
+    total = sum((x for x in range(0, 1000000)))
+
+
+def main():
+    # You'll need to change this to be the absolute path to the root folder
+    # of the dataset
+    data_directory = "/Users/mihaliskoutouvos/Downloads/final_pickles/hash_index.pkl"
+
+    # Here, we are creating a sample binary search tree index object
+    # and sending it to the index_files function
+    hm_index = HashMapIndex()
+    index_files(data_directory, hm_index)
+
+    # As a gut check, we are printing the keys that were added to the
+    # index in order.
+    print(hm_index.get_keys_in_order())
+
+    # quick demo of how to use the timing decorator included
+    # in indexer.util
+    loopy_loop()
+
+
+if __name__ == "__main__":
+    main()
