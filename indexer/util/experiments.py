44f6842b7dc1dd1f6cd7ff747dd8589e7d23f520
Mihalis Koutouvos <mihaliskoutouvos@gmail.com>
2025-02-01 16:14:32 -0500

    Added some working experiments for E1-3. Trying to implement the pickle info

--- indexer/util/experiments.py
+++ indexer/util/experiments.py
@@ -6,6 +6,7 @@ from indexer.trees.bst_index import BinarySearchTreeIndex
 from indexer.util.timer import timer
 import random
 import string
+import pickle
 
 # Generate random strings
 def generate_random_string(length):
@@ -32,30 +33,16 @@ def experiment_search_existing(index, n):
 # Insert 100 keys first (so they exist before searching)
 for i in range(100):
     bst_index.insert(f"term{i}", f"value{i}")
-
-# Insert 100 keys first (so they exist before searching)
-for i in range(100):
     avl_index.insert(f"term{i}", f"value{i}")
-
-# Insert 100 keys first (so they exist before searching)
-for i in range(100):
     hm_index.insert(f"term{i}", f"value{i}")
-
-# Insert 100 keys first (so they exist before searching)
-for i in range(100):
     l_index.insert(f"term{i}", f"value{i}")
 
 # Now perform search experiments
+print("E1 Experiments")
 experiment_search_existing(bst_index, 100)
 experiment_search_existing(avl_index, 100)
 experiment_search_existing(hm_index, 100)
 experiment_search_existing(l_index, 100)
-
-
-
-
-
-
 
 # E2: Search time for non-existing elements
 @timer
@@ -63,19 +50,33 @@ def experiment_search_non_existing(index, n):
     search_times = []
     for i in range(n):
         term = f"non_existing_term{i}"
-        search_time = index.search(term)
-        search_times.append(search_time)
+        try: 
+            search_time = index.search(term)
+            search_times.append(search_time)
+        except KeyError: 
+            pass
     return search_times
 
 # Now perform search experiments
+print("E2 Experiments")
 experiment_search_non_existing(bst_index, 100)
 experiment_search_non_existing(avl_index, 100)
 experiment_search_non_existing(hm_index, 100)
 experiment_search_non_existing(l_index, 100)
 
 
-#E3: 
+#E3: Check Inserting Time
+@timer 
+def insert_items(index, n):
+    for i in range(n):
+        key = generate_random_string(12)
+        value = generate_random_string(12)
+        index.insert(key, value)
 
 
-
-
+# Now perform search experiments
+print("E3 Experiments")
+insert_items(bst_index, 100)
+insert_items(avl_index, 100)
+insert_items(hm_index, 100)
+insert_items(l_index, 100)
