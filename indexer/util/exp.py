8a8fe41fa44ab233051031c6485198b7c32f4e11
lilyhoffman <lilynaavhoffman@gmail.com>
2025-02-04 09:52:16 -0500

    added 4 columns to each func

--- indexer/util/exp.py
+++ indexer/util/exp.py
@@ -9,6 +9,7 @@ import pickle
 import time
 import pandas as pd
 from typing import List
+from openpyxl import load_workbook
 
 bst_index = BinarySearchTreeIndex()
 avl_index = AVLTreeIndex()
@@ -18,7 +19,7 @@ l_index = ListIndex()
 
 # E1: Search time for existing elements
 
-def experiment_searching(index_name, index, datasets, n_list):
+def experiment_searching(index_name, index, datasets, n_list, run_id, compute_proc_type, primary_memory_size):
     # Initialize counters for the final counts
     total_docs_indexed = 0
     total_tokens_indexed = 0
@@ -58,7 +59,10 @@ def experiment_searching(index_name, index, datasets, n_list):
             'n': n_list[i],
             'total_docs_indexed': total_docs_indexed,
             'total_tokens_indexed': total_tokens_indexed,
-             'search_time (in seconds)': round(search_time, 6) 
+             'search_time (in seconds)': round(search_time, 6),
+             'run_id': run_id,
+             'compute_proc_type': compute_proc_type,
+             'primary_memory_size': primary_memory_size
         }
 
         # Convert data to a DataFrame (one row)
@@ -71,7 +75,7 @@ def experiment_searching(index_name, index, datasets, n_list):
 
 
 # E2: Search time for non-existing elements
-def experiment_missing_words(index_name, index, datasets, n_list):
+def experiment_missing_words(index_name, index, datasets, n_list, run_id, compute_proc_type, primary_memory_size):
     """
     Identifies the number of words in datasets that are not found in the given index and records the time taken.
     """
@@ -98,7 +102,10 @@ def experiment_missing_words(index_name, index, datasets, n_list):
             'dataset': [i + 1],
             'n': [n_list[i]],
             'unindexed_word_count': [unindexed_word_count],
-            'search_time (in seconds)': [search_time] 
+            'search_time (in seconds)': [search_time],
+            'run_id': run_id,
+             'compute_proc_type': compute_proc_type,
+             'primary_memory_size': primary_memory_size 
         })
         
         # Append to the main DataFrame
@@ -108,7 +115,7 @@ def experiment_missing_words(index_name, index, datasets, n_list):
 
 
 #E3 - Finding phrases (two words) in a search dataset
-def find_phrases_in_datasets(index_name, index, datasets, n_list):
+def find_phrases_in_datasets(index_name, index, datasets, n_list, run_id, compute_proc_type, primary_memory_size):
     """
     Identifies the number of documents with the exact phrase of words
     """
@@ -163,7 +170,10 @@ def find_phrases_in_datasets(index_name, index, datasets, n_list):
             'n': n_list[i],
             'total_docs_indexed': total_docs_indexed,
             'total_tokens_indexed': total_tokens_indexed,
-             'search_time (in seconds)': round(search_time, 6) 
+             'search_time (in seconds)': round(search_time, 6),
+            'run_id': run_id,
+             'compute_proc_type': compute_proc_type,
+             'primary_memory_size': primary_memory_size 
         }
 
         # Convert data to a DataFrame (one row)
@@ -173,7 +183,6 @@ def find_phrases_in_datasets(index_name, index, datasets, n_list):
         df = pd.concat([df, new_row], ignore_index=True)
 
     return df
-
 
 def find_search_data_sets(path: str):
     all_search_datasets = []
@@ -208,76 +217,79 @@ def my_load_index(file_path):
 
 def main():
 
-    #pickle_data_bst = '/Users/Heidi/Downloads/final_pickles/bst_index.pkl'
-    #pickle_data_avl = '/Users/Heidi/Downloads/final_pickles/avl_index.pkl'
-    #pickle_data_ht = '/Users/Heidi/Downloads/final_pickles/hash_index.pkl'
-    #pickle_data_l = '/Users/Heidi/Downloads/final_pickles/list_index.pkl'
+    # pickle_data_bst = '/Users/Heidi/Downloads/final_pickles/bst_index.pkl'
+    # pickle_data_avl = '/Users/Heidi/Downloads/final_pickles/avl_index.pkl'
+    # pickle_data_ht = '/Users/Heidi/Downloads/final_pickles/hash_index.pkl'
+    # pickle_data_l = '/Users/Heidi/Downloads/final_pickles/list_index.pkl'
 
-    #pickle_data_bst = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\bst_index.pkl'
-    #pickle_data_avl = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\avl_index.pkl'
-    #pickle_data_ht = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\hash_index.pkl'
-    #pickle_data_l = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\list_index.pkl'
+    pickle_data_bst = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\bst_index.pkl'
+    pickle_data_avl = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\avl_index.pkl'
+    pickle_data_ht = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\hash_index.pkl'
+    pickle_data_l = 'C:\\Users\\lilyh\\Downloads\\final_pickles_results\\final_pickles\\list_index.pkl'
 
-    #pickle_data_bst = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/bst_index.pkl'
-    #pickle_data_avl = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/avl_index.pkl'
-    #pickle_data_ht = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/hash_index.pkl'
-    pickle_data_l = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/list_index.pkl'
-       
+    # pickle_data_bst = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/bst_index.pkl'
+    # pickle_data_avl = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/avl_index.pkl'
+    # pickle_data_ht = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/hash_index.pkl'
+    # pickle_data_l = '/Users/mihaliskoutouvos/Downloads/final_pickles 3/list_index.pkl'
 
-    #bst_index = my_load_index(pickle_data_bst)
-    #avl_index = my_load_index(pickle_data_avl)
-    #hash_index = my_load_index(pickle_data_ht)
+
+    bst_index = my_load_index(pickle_data_bst)
+    avl_index = my_load_index(pickle_data_avl)
+    hash_index = my_load_index(pickle_data_ht)
     list_index = my_load_index(pickle_data_l)
 
 
     # data_directory = '/Users/Heidi/Downloads/compiled_datasets_final.json'
-    #data_directory = 'C:\\Users\\lilyh\\Downloads\\experiment_data\\compiled_datasets_final.json'
-    data_directory = '/Users/mihaliskoutouvos/Desktop/Classes/24s-ds4300-koutouvos/practical-01-index_builder/compiled_datasets_final.json'
+    data_directory = 'C:\\Users\\lilyh\\Downloads\\experiment_data\\compiled_datasets_final.json'
+    # data_directory = '/Users/mihaliskoutouvos/Desktop/Classes/24s-ds4300-koutouvos/practical-01-index_builder/compiled_datasets_final.json'
 
     # make a list of all the words from search data sets
     datasets, n_list = find_search_data_sets(data_directory)
 
-    
-
     # Now perform search experiments
-    #print("E1 Experiments")
-    #df1 = experiment_searching('list', list_index, datasets, n_list)
-    #df2 = experiment_searching('hash', hash_index, datasets, n_list)
-    #df3 = experiment_searching('avl', avl_index, datasets, n_list)
-    #df4 = experiment_searching('bst', bst_index, datasets, n_list)
+    # print("E1 Experiments")
+    # df1 = experiment_searching('list', list_index, datasets, n_list, 1, 'Intel i5', 16)
+    # df2 = experiment_searching('hash', hash_index, datasets, n_list, 1, 'Intel i5', 16)
+    # df3 = experiment_searching('avl', avl_index, datasets, n_list, 1, 'Intel i5', 16)
+    # df4 = experiment_searching('bst', bst_index, datasets, n_list, 1, 'Intel i5', 16)
 
-    #print(df1)
-    #print(df2)
-    #print(df3)
-    #print(df4)
 
     #print("E2 Experiments")
     # df1 = experiment_missing_words('list', list_index, datasets, n_list)
     # df2 = experiment_missing_words('hash', hash_index, datasets, n_list)
-    #df3 = experiment_missing_words('avl', avl_index, datasets, n_list)
-    #df4 = experiment_missing_words('bst', bst_index, datasets, n_list)
+    # df3 = experiment_missing_words('avl', avl_index, datasets, n_list)
+    # df4 = experiment_missing_words('bst', bst_index, datasets, n_list)
 
-    
+    # df1 = experiment_missing_words('list', list_index, datasets, n_list, 2, 'Intel i5', 16)
+    # df2 = experiment_missing_words('hash', hash_index, datasets, n_list, 2, 'Intel i5', 16)
+    # df3 = experiment_missing_words('avl', avl_index, datasets, n_list, 2, 'Intel i5', 16)
+    # df4 = experiment_missing_words('bst', bst_index, datasets, n_list, 2, 'Intel i5', 16)
+
+    print("E3 Experiments")
     #df1 = experiment_missing_words('list', list_index, datasets, n_list)
     #df2 = experiment_missing_words('hash', hash_index, datasets, n_list)
     #df3 = experiment_missing_words('avl', avl_index, datasets, n_list)
     #df4 = experiment_missing_words('bst', bst_index, datasets, n_list)
 
-    #print(df1)
-    #print(df2)
-    #print(df3)
-    #print(df4)
+    # df1 = experiment_missing_words('list', list_index, datasets, n_list, 3, 'Intel i5', 16)
+    # df2 = experiment_missing_words('hash', hash_index, datasets, n_list, 3, 'Intel i5', 16)
+    # df3 = experiment_missing_words('avl', avl_index, datasets, n_list, 3, 'Intel i5', 16)
+    # df4 = experiment_missing_words('bst', bst_index, datasets, n_list, 3, 'Intel i5', 16)
 
-    print("E3 Experiments")
-    df1 = find_phrases_in_datasets('list', list_index, datasets, n_list)
-    #df2 = find_phrases_in_datasets('hash', hash_index, datasets, n_list)
-    #df3 = find_phrases_in_datasets('avl', avl_index, datasets, n_list)
-    #df4 = find_phrases_in_datasets('bst', bst_index, datasets, n_list)
+    # df1 = find_phrases_in_datasets('list', list_index, datasets, n_list)
+    # df2 = find_phrases_in_datasets('hash', hash_index, datasets, n_list)
+    # df3 = find_phrases_in_datasets('avl', avl_index, datasets, n_list)
+    # df4 = find_phrases_in_datasets('bst', bst_index, datasets, n_list)
+
+    df1 = find_phrases_in_datasets('list', list_index, datasets, n_list, 3, 'Intel i5', 16)
+    df2 = find_phrases_in_datasets('hash', hash_index, datasets, n_list, 3, 'Intel i5', 16)
+    df3 = find_phrases_in_datasets('avl', avl_index, datasets, n_list, 3, 'Intel i5', 16)
+    df4 = find_phrases_in_datasets('bst', bst_index, datasets, n_list, 3, 'Intel i5', 16)
 #
-
-    #df_combined = pd.concat([df2, df4], axis=0)
-    print(df1)
-    #print(df2)
+    # print(df2)
+    df_combined = pd.concat([df1, df2, df3, df4], axis=0)
+    df_combined.to_excel('output_exp3.xlsx', index=False, sheet_name='sheet1')
+    
     #print(df3)
     #print(df4)
 
@@ -291,6 +303,20 @@ def main():
     experiment_search_non_existing(avl_index, 100)
     experiment_search_non_existing(hm_index, 100)
     experiment_search_non_existing(l_index, 100)'''
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
